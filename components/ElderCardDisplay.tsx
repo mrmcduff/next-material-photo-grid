8f40a65b6157692bd12ff75d1a589cf1c5d7c6d9
Michael McDuffee <mrmcduff@gmail.com>
2020-04-22 18:00:29 -0700

    show linear loading indicator when downloading photo

--- components/ElderCardDisplay.tsx
+++ components/ElderCardDisplay.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 interface Props {
     imageUrl?: string;
@@ -39,10 +40,19 @@ const useStyles = makeStyles({
 
 const ElderCardDisplay: React.FC<Props> = ({ name, setName, text, type, imageUrl }) => {
     const classes = useStyles();
+    const [loading, setLoading] = useState(true);
     return (
         <Card className={classes.root}>
             <CardContent>
-                <CardMedia component="img" className={classes.media} image={imageUrl} />
+                <CardMedia
+                    component="img"
+                    className={classes.media}
+                    image={imageUrl}
+                    onLoad={() => setLoading(false)}
+                />
+                {
+                    loading && <LinearProgress color="secondary" />
+                }
                 <Typography variant="h5" gutterBottom>
                     {name}
                 </Typography>
